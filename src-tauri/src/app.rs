8a2bb196bd51b10692ecac1ad0cc297ab7fdd595
DISBAIC <LC13845295251@Outlook.com>
2026-06-19 02:33:05 +0800

    feat: configure tauri updater

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -1728,6 +1728,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             list_instances,
             list_presets,
@@ -1748,6 +1749,5 @@ pub fn run() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
 
 
